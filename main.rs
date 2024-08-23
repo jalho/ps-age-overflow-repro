dd394c8da7e7f419226429105005e7cbabcc9220
jalho <alho.jere@gmail.com>
2024-08-23 19:42:43 +0300

    Check all used PIDs

--- main.rs
+++ main.rs
@@ -29,21 +29,30 @@ fn main() {
         let pid_foo = rx.recv().expect("cannot receive id of process './foo'");
         pids[attempt] = Some(pid_foo);
 
-        let output = Command::new("ps")
-            .args(["-o", "etimes=", "-p", &pid_foo.to_string()])
-            .stdout(Stdio::piped())
-            .output()
-            .expect("cannot get output of process 'ps'");
-        assert_eq!(
-            String::from_utf8_lossy(&output.stdout),
-            "      0\n",
-            "at attempt #{}",
-            attempt + 1
-        );
+        for &pid in pids.iter().flatten() {
+            let output = Command::new("ps")
+                .args(["-o", "etimes=", "-p", &pid.to_string()])
+                .stdout(Stdio::piped())
+                .output()
+                .expect("cannot get output of process 'ps'");
+            let stdout_utf8 = String::from_utf8_lossy(&output.stdout);
+            let stdout_trimmed = stdout_utf8.trim();
+            let stdout_u64 = stdout_trimmed
+                .parse::<u64>()
+                .expect(&format!("cannot parse stdout as u64: \"{}\"", stdout_utf8));
+            assert_eq!(
+                stdout_u64 < 60,
+                true,
+                "got age {}s at attempt #{}",
+                stdout_u64,
+                attempt + 1,
+            );
+        }
     }
 
     println!(
-        "Attempted {} times but couldn't reproduce the issue! PIDS used by executable './foo': {:?}",
-        ATTEMPTS, pids
+        "Attempted {} times but couldn't reproduce the issue! PIDS used by executable './foo': {}",
+        ATTEMPTS,
+        pids.map(|n| { n.unwrap().to_string() }).join(", ")
     );
 }
