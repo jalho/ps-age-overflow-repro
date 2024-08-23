55de48c435871c3fffdd22c1a470aea2220f57ad
jalho <alho.jere@gmail.com>
2024-08-23 18:43:16 +0300

    Implement minimal repro

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # ps age overflow
 
-Description in detail: https://github.com/jalho/rds-daemon/issues/1
-
-## minimal repro
+```
+$ ps --version
+ps from procps-ng 4.0.2
+```
 
 ```
 $ rustc --version
 rustc 1.79.0 (129f3b996 2024-06-10)
+
 $ rustc main.rs
+
 $ ./main
+thread 'main' panicked at main.rs:46:13:
+assertion `left == right` failed: at attempt #7
+  left: "4123168608\n"
+ right: "      0\n"
 ```
+
+The demo repeatedly starts a process from executable [`./foo`](./foo), reads its
+age (using external command `ps -o etimes= -p $PID`) and kills it (using
+external command `kill`). Each subsequent attempt to reproduce the issue starts
+with the kill command so the process' age is expected to always be close to 0.
+The issue is that sometimes the age is reported to be 4123168608, which is a
+little over four billion, hence the hint of an overflow in the title.
--- main.rs
+++ main.rs
@@ -1,3 +1,61 @@
+use std::process::*;
+use std::sync::*;
+use std::thread::*;
+
 fn main() {
-  println!("Hello world!");
+    let mut attempts = 0;
+    let pid_foo: Option<u32> = None;
+
+    loop {
+        let (tx, rx) = mpsc::channel::<u32>();
+
+        if attempts >= 100 {
+            break;
+        }
+        attempts += 1;
+
+        match pid_foo {
+            Some(pid) => {
+                _ = Command::new("kill")
+                    .arg(&pid.to_string())
+                    .spawn()
+                    .expect("cannot not spawn process 'kill'")
+                    .wait()
+                    .expect("cannot wait for termination of process 'kill'");
+            }
+            None => {}
+        }
+
+        spawn(move || {
+            let process_foo = Command::new("./foo")
+                .spawn()
+                .expect("cannot not spawn process './foo'");
+            // Not waiting for the process "./foo" to terminate!
+            let pid = process_foo.id();
+            tx.send(pid).expect("cannot send id of process './foo'");
+        });
+        let pid_foo = Some(rx.recv().expect("cannot receive id of process './foo'"));
+
+        match pid_foo {
+            Some(pid) => {
+                let output = Command::new("ps")
+                    .args(["-o", "etimes=", "-p", &pid.to_string()])
+                    .stdout(Stdio::piped())
+                    .output()
+                    .expect("cannot get output of process 'ps'");
+                assert_eq!(
+                    String::from_utf8_lossy(&output.stdout),
+                    "      0\n",
+                    "at attempt #{}",
+                    attempts
+                );
+            }
+            None => {}
+        }
+    }
+
+    println!(
+        "Attempted {} times but couldn't reproduce the issue!",
+        attempts
+    );
 }
